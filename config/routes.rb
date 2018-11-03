82ca922937592ff36ad4ae9c38f5130474d9885f
starmie <minminsu0033@gmail.com>
2018-11-03 04:10:22 +0000

    toppage navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'toppages#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
 end
