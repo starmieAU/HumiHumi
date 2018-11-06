fa6634a9afdca6fcf4f1a8ef032a09281b927596
starmie <minminsu0033@gmail.com>
2018-11-06 12:00:58 +0000

    login bagfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     
-    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.uid
     flash[:success] = "ユーザ認証が完了しました。"
     redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ApplicationRecord
   has_many :follower_relations, -> { where(follow: true) }, class_name: 'UserRelation', foreign_key: 'to_user_id'
   has_many :followers, through: :follower_relations, source: :user
   
+  def get_shelf(book)
+    self.shelf_relations.find_by(book_id: book.id)
+  end
+  
   #shelf
   def shelf(book)
     self.shelf_relations.find_or_create_by(book_id: book.id)
@@ -90,16 +94,20 @@ class User < ApplicationRecord
   end
   
   #auth認証
-  def self.find_or_create_from_auth(auth)
-    provider = auth[:provider]
-    uid = auth[:uid]
-    auth_name = auth[:info][:name]
-    auth_nickname = auth[:info][:name]
-    image_url = auth[:info][:image].gsub('_normal', '')
-    self.find_or_create_by(provider: provider, uid: uid) do |user|
+  def self.find_or_create_from_auth_hash(auth_hash)
+    #OmniAuthで取得した各データを代入していく
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    auth_name = auth_hash[:info][:name]
+    auth_nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image].gsub('_normal', '')
+
+    User.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.prof_name = auth_name
       user.auth_name = auth_name
       user.auth_nickname = auth_nickname
       user.image_url = image_url
     end
   end
+  
 end
