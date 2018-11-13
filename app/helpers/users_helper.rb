6980dd108ac06dd4482e8d1a7eea2812d32e594f
starmie <minminsu0033@gmail.com>
2018-11-13 15:34:39 +0000

    tweet修正01

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
   def tweet_str(review)
-    "#{review.user.prof_name}さんの10文字レビュー\n\n【#{review.review_10_char}】\n#{review.book.title}\n#{request.url}"
+    if review.review_10_char.present?
+      "#{review.user.prof_name}さんの10文字レビュー\n\n【#{review.review_10_char}】\n#{review.book.title}\n#{request.url}"
+    else
+      "#{review.user.prof_name}さんレビューページ\n#{review.book.title}\n\n#{request.url}"
+    end
   end
 end
