0af79d279f4d1c6624405a62ada251d0da027b2f
starmie <minminsu0033@gmail.com>
2018-11-16 17:08:04 +0000

    全体/ユーザーtimeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,17 @@
 class ToppagesController < ApplicationController
   def index
-    @microposts = Review.where(micropost_f: true).order(updated_at: "DESC").page(params[:page]).per(10)
+    if logged_in?
+      @microposts = Review.where(micropost_f: true,user_id: current_user.followings + [current_user] - current_user
+      .mutings ).order(updated_at: "DESC").page(params[:page]).per(10)
+    else
+      @microposts = Review.where(micropost_f: true).order(updated_at: "DESC").page(params[:page]).per(10)
+    end
   end
   def what
-    
   end
+  def index_all
+    @microposts = Review.where(micropost_f: true).order(updated_at: "DESC").page(params[:page]).per(10)
+    render :index
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
+  resources :toppages, only: [:index] do
+    collection do
+      get :index_all
+    end
+  end
   root 'toppages#index'
   get '/humihumi', to: 'toppages#what'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
-  resources :books, only: [:show, :new]
-  resources :reviews, only: [:show, :edit, :destroy, :create, :index, :update]
+  resources :books, only: [:show, :new] do
+    member do
+      get :favorite_users
+      get :shelf_users
+    end
+  end
+  resources :reviews, only: [:show, :edit, :destroy, :create, :index, :update] do
+    collection do
+      get :rank_shelf
+      get :rank_favorite
+      get :rank_short
+      get :rank_long
+    end
+  end
   resources :users, only: [:index, :show, :edit, :update] do
     member do
       get :followings
