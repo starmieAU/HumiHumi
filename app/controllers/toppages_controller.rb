208d6ad4eb3cc1bf69cf2fa5c010ea2550d098e2
starmie <minminsu0033@gmail.com>
2018-12-09 12:41:52 +0000

    pngファイル階層

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -19,7 +19,7 @@ class ToppagesController < ApplicationController
   
   def get_tweet_data
     {
-      card: "summary",
+      card: "summary_large_image",
       title: "書籍レビューサイト【HumiHumi】",
       content: "次の本は何を読もうか？HumiHumiで「私の三冊」「10文字レビュー」等ユニークな機能を使ってみんなのレビューを見てみよう！",
       image_url: "/images/top_image.png"
